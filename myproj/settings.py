a4d9cb3592701e4a2031fe11264a57d1cf4a425d
Shaked Goren <117649992+shakedgoren@users.noreply.github.com>
2023-07-03 13:01:40 +0300

    Update settings.py

--- myproj/settings.py
+++ myproj/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i28&54ev_su(dgaj$82v!qxin_d79@f%(195xzb&h68mgug+ww'
+SECRET_KEY = ''
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
